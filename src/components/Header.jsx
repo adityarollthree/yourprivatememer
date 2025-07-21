05b0a2ce714b9fcc06c95dca1fe40d794da0d758
Aditya <rajaditya839@gmail.com>
2025-07-22 00:40:21 +0530

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+// import trollFace from "../images/troll-face.png";
+export default function Header() {
+  return (
+    <header>
+      <img
+        src="https://media.tenor.com/Kj-8F_GmaGMAAAAi/%D0%BB%D1%8E%D0%B1%D0%BB%D1%8E.gif"
+        alt="logo image"
+      />
+      <span>Meme Generator</span>
+    </header>
+  );
+}
